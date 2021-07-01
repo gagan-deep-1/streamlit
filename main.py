d436b9dd8a781bbf2fb65fdd107023530594b659
Gagan <gagandeep@graphenesvc.com>
2021-07-01 11:49:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,9 @@ try:
 	with col1:
 		selected_indices = st.selectbox('Select Entity:', list(set(list(data["Entity"]))))
 		st.write("##### Entity Table", data1)
-	
+	data2 = pd.DataFrame(aliases[selected_indices],columns =['Alias',"Relevance(Alias)",'Frequency','Entity support count'])
 	with col2:
 		selected_indices = st.selectbox('Select Alias:', list(set(list(data2["Alias"]))))
-		data2 = pd.DataFrame(aliases[selected_indices],columns =['Alias',"Relevance(Alias)",'Frequency','Entity support count'])
 		st.write("##### Alias Table", data2)
 	data3 = pd.DataFrame(contexts[selected_indices],columns =['context',"node id"])
 	st.write("#### Contexts for Alias:",selected_indices)
