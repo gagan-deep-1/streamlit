d9c435858cde879f6dc6fba4d6f797218dd9dc57
Gagan <gagandeep@graphenesvc.com>
2021-07-12 09:26:39 +0530

    Update EntitiesnAliasesInfluenster.py, EntitiesnAliasesTweets.py, and 7 more files...

--- main.py
+++ main.py
@@ -24,19 +24,24 @@ def loadData(path):
 
 try:
 	st.write("# Entities and Aliases")
-	entities, aliases, contexts = loadData("alias_detection_demo_APDO_fulltweets.json")
+	entities, aliases, contexts = loadData("alias_detection_demo_APDO_influenster.json")
 	col1, col2 = st.beta_columns(2)
-	data = pd.DataFrame(entities,columns =['Entity',"Relevance(entity)",'Frequency','CNT'])
+	data = pd.DataFrame(entities,columns =['Entity',"Relevance",'Frequency','CNT'])
 	data1 = data
+	data1 = data1.style.set_properties(**{'font-size': '14pt',})
 	with col1:
 		selected_indices = st.selectbox('Select Entity:', list(set(list(data["Entity"]))))
-		st.write("##### Entity Table", data1)
+		st.write("##### Entity Table")
+		st.dataframe(data=data1, height=300)
 	data2 = pd.DataFrame(aliases[selected_indices],columns =['Alias',"Relevance(Alias)",'Frequency','Entity support count'])
+
 	with col2:
 		selected_indices1 = st.selectbox('Select Alias:', list(set(list(data2["Alias"]))))
+		data2 = data2.style.set_properties(**{'font-size': '14pt',})
 		st.write("##### Alias Table for entity : ",selected_indices, data2)
 	data3 = pd.DataFrame(contexts[selected_indices1],columns =['context',"node id"])
 	st.write("#### Contexts for Alias:",selected_indices1)
+	data3 = data3.style.set_properties(**{'font-size': '14pt',})
 	st.table(data3)
 	#st.write("Context Table", data3)
 except Exception as e:
